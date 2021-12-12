7a074593fac6097af845117ed649b023ed6672a8
Ugur Muslim <ugur.muslim@gmail.com>
2021-12-12 23:24:21 +0300

    contineu

--- Modules/Home/Resources/views/Article/kripto.blade.php
+++ Modules/Home/Resources/views/Article/kripto.blade.php
@@ -6,19 +6,21 @@
     <div class="container">
         <div class="row">
         @foreach($articlesDB["Kripto"]->slice(7)->take(40) as $article)
-                <div class="col-md-24 col-lg-12 p-4 ">
+                <div class="col-md-6">
                     <a href="{{route('article.show',['slug' => $article->slug ])}}">
-                        <div class="row">
-                            <div class="col-10 bg-orange crypto-wide-section">
-                                <div class="crypto-title" style="color: black;">
+                        <div class="col-24  bg-dark">
+                            <div class="col-sm-24 ">
+                                <div class="col-24 crypto crypto-md"
+                                     style="background-image: url({{asset($article->image_path)}})"></div>
+                                <div class="crypto-title">
                                     {{ \Illuminate\Support\Str::limit($article->title, 70, $end='...') }}
+                                    <div class="crypto-text-bottom-sm">
+                                    <span
+                                        class="text-white">{{ Carbon\Carbon::parse($article->created_at)->format('d F')}}</span><span> • {{ Carbon\Carbon::parse($article->created_at)->format('H:m')}} • by parafesor</span>
+                                    </div>
                                 </div>
-                                <div class="crypto-text-bottom-sm">
-                                    <span>{{ Carbon\Carbon::parse($article->created_at)->format('d F')}} • {{ Carbon\Carbon::parse($article->created_at)->format('H:m')}} • by parafesor</span>
-                                </div>
+
                             </div>
-                            <div class="col-14"
-                                 style="background-image: url(https://i4.hurimg.com/i/hurriyet/75/0x0/61821b494e3fe113306aabb2.jpg)"></div>
                         </div>
                     </a>
                 </div>
