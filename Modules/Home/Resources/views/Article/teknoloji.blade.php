3f6597fa7e5c45e33395701b0ed9600129b96462
Ugur Muslim <ugur.muslim@gmail.com>
2021-12-12 23:02:58 +0300

    contineu

--- Modules/Home/Resources/views/Article/teknoloji.blade.php
+++ Modules/Home/Resources/views/Article/teknoloji.blade.php
@@ -3,6 +3,26 @@
 @section('content')
     @include('home::partials._header')
     @include('home::sections.teknoloji')
+    <div class="container">
+        <div class="row">
+            @foreach($articles["Teknoloji"][\App\Parafesor\Constants\CategorySectionTypes::NORMAL]->slice(8)->take(40) as $article)
+                <div class="col-sm-24 col-md-12 col-lg-8 mt-3">
+                    <a href="{{route('article.show',['slug' => $article->slug ])}}">
+                        <div class="col-24 tech tech-md"
+                             style="background-image: url({{asset($article->image_path)}})"></div>
+                        <div class="tech-title">{{$article->title}}
+                            <div class="tech-text-bottom-sm">
+                                    <span
+                                        class="text-purple">{{ Carbon\Carbon::parse($article->created_at)->format('d F')}}</span><span> • {{ Carbon\Carbon::parse($article->created_at)->format('H:m')}} • by parafesor</span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
+
+
+        </div>
+    </div>
 
 
     @include('home::partials._javascript')
