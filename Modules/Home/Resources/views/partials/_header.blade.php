2c93b77af19a4ccc38ba13598058d504ada80ac1
Ugur Muslim <ugur.muslim@gmail.com>
2021-12-12 23:31:37 +0300

    contineu

--- Modules/Home/Resources/views/partials/_header.blade.php
+++ Modules/Home/Resources/views/partials/_header.blade.php
@@ -154,7 +154,7 @@
                     </div>
 
                     <div class="col-md-12">
-                        <div class="col-md-24 bg-dark-orange mt-4 match" matchTo="internet-second-row">
+                        <div class="col-md-24 bg-dark-orange mt-4">
                             <div class="col-md-24 bg-white h-100 tech-box">
                                 <div class="tech-news-box-image"
                                      style="background-image: url(https://i2.cnnturk.com/i/cnnturk/75/800x400/5f4642f9214ed8165ce5a0ae)"></div>
@@ -169,7 +169,7 @@
                     </div>
 
                     <div class="col-md-8">
-                        <div class="col-md-24 bg-dark-orange mt-4 match" matchTo="internet-second-row">
+                        <div class="col-md-24 bg-dark-orange mt-4" >
                             <div class="col-md-24 bg-white h-100 tech-box">
                                 <div class="tech-news-box-image"
                                      style="background-image: url(https://i2.cnnturk.com/i/cnnturk/75/800x400/5f4642f9214ed8165ce5a0ae)"></div>
