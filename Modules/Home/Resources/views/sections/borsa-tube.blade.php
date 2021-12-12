346cc51ed9389463862cfc475dc0115f1234e629
Ugur Muslim <ugur.muslim@gmail.com>
2021-12-12 22:18:22 +0300

    continue

--- Modules/Home/Resources/views/sections/borsa-tube.blade.php
+++ Modules/Home/Resources/views/sections/borsa-tube.blade.php
@@ -13,10 +13,10 @@
         <div class="row text-white">
 
             <div class="col-lg-14">
-                <div class="ratio ratio-16x9 mt-2" id="borsaEmbed">
+                <div class="ratio ratio-16x9 mt-2" id="borsaEmbed" ratio="0.55">
                     @foreach($articles["Borsa Tube"][\App\Parafesor\Constants\CategorySectionTypes::MAIN_SLIDER]->take(1) as $article)
                         <div class="image-card image-card-bw-16x10"
-                             style="background-image: url({{asset($article->image_path)}})"></div>
+                             style="    background-image: url({{asset($article->image_path)}})"></div>
 
                         {{--<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" allowfullscreen></iframe>--}}
                     @endforeach
