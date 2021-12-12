f48be59ecd84ae11612936d859f67221acfc242a
Ugur Muslim <ugur.muslim@gmail.com>
2021-12-12 23:22:16 +0300

    contineu

--- Modules/Home/Resources/views/Article/netkolik.blade.php
+++ Modules/Home/Resources/views/Article/netkolik.blade.php
@@ -5,25 +5,27 @@
     @include('home::sections.netkolik')
     <div class="container">
         <div class="row">
-        @foreach($articlesDB["Netkolik"]->slice(6)->take(20) as $article)
-
-            <!-- First Small News of the section -->
-                <div class="col-lg-6 col-sm-12 mt-5">
+        @foreach($articlesDB["Netkolik"]->slice(8)->take(40) as $article)
+                <div class="col-md-8 mt-3">
                     <a href="{{route('article.show',['slug' => $article->slug ])}}">
-                        <div class="card news-card news-card-small ">
-                            <div class="news-card-img-container bg-light-grey">
-                                <div style="background: url({{asset($article->image_path)}})" alt=""
-                                     class="news-img"></div>
-                                <div class="news-card-img-text">{{$article->title}}</div>
-                            </div>
-                            <div class="news-card-bottom">
+
+                        <div class="col-24">
+                            <div class="col-sm-24 ">
+                                <div class="col-24 internet internet-md"
+                                     style="background-image: url({{asset($article->image_path)}})"></div>
+                                <div class="internet-title">
+                                    <div class="multilineEllipsis" multilineEllipsisMax="100">{{$article->title}}
+                                    </div>
+                                    <div class="card-bottom-date">
                                     <span
-                                        class="text-danger">{{ Carbon\Carbon::parse($article->created_at)->format('d F')}}</span><span> • {{ Carbon\Carbon::parse($article->created_at)->format('H:m')}} • by parafesor</span>
+                                        class="text-dark-orange">{{Carbon\Carbon::parse($article->created_at)->format('d F')}}</span><span> • {{ Carbon\Carbon::parse($article->created_at)->format('H:m')}} • by parafesor</span>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </a>
                 </div>
-                <!-- First Small News of the section -->
             @endforeach
 
 
