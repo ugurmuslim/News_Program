d2ecea17ef6727f12e56ced5d2e98b3f2806f3db
Ugur Muslim <ugur.muslim@gmail.com>
2021-12-22 16:06:30 +0300

    conitnue

--- Modules/Admin/Resources/views/partials/_nav.blade.php
+++ Modules/Admin/Resources/views/partials/_nav.blade.php
@@ -25,94 +25,95 @@
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <!-- Navbar Search -->
-        {{--<li class="nav-item">
-            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                <i class="fas fa-search"></i>
-            </a>
-            <div class="navbar-search-block">
-                <form class="form-inline">
-                    <div class="input-group input-group-sm">
-                        <input class="form-control form-control-navbar" type="search" placeholder="Search"
-                               aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-navbar" type="submit">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
+    {{--<li class="nav-item">
+        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+            <i class="fas fa-search"></i>
+        </a>
+        <div class="navbar-search-block">
+            <form class="form-inline">
+                <div class="input-group input-group-sm">
+                    <input class="form-control form-control-navbar" type="search" placeholder="Search"
+                           aria-label="Search">
+                    <div class="input-group-append">
+                        <button class="btn btn-navbar" type="submit">
+                            <i class="fas fa-search"></i>
+                        </button>
+                        <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                            <i class="fas fa-times"></i>
+                        </button>
                     </div>
-                </form>
-            </div>
-        </li>--}}
+                </div>
+            </form>
+        </div>
+    </li>--}}
 
-        <!-- Messages Dropdown Menu -->
-     {{--   <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
-                <i class="far fa-comments"></i>
-                <span class="badge badge-danger navbar-badge">3</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-                        <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                                Brad Diesel
-                                <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm">Call me whenever you can...</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                        </div>
-                    </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-                        <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                                John Pierce
-                                <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm">I got your message bro</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                        </div>
-                    </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-                        <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                                Nora Silvester
-                                <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm">The subject goes here</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                        </div>
-                    </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-            </div>
-        </li>--}}
-        <!-- Notifications Dropdown Menu -->
+    <!-- Messages Dropdown Menu -->
+    {{--   <li class="nav-item dropdown">
+           <a class="nav-link" data-toggle="dropdown" href="#">
+               <i class="far fa-comments"></i>
+               <span class="badge badge-danger navbar-badge">3</span>
+           </a>
+           <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+               <a href="#" class="dropdown-item">
+                   <!-- Message Start -->
+                   <div class="media">
+                       <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                       <div class="media-body">
+                           <h3 class="dropdown-item-title">
+                               Brad Diesel
+                               <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                           </h3>
+                           <p class="text-sm">Call me whenever you can...</p>
+                           <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                       </div>
+                   </div>
+                   <!-- Message End -->
+               </a>
+               <div class="dropdown-divider"></div>
+               <a href="#" class="dropdown-item">
+                   <!-- Message Start -->
+                   <div class="media">
+                       <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                       <div class="media-body">
+                           <h3 class="dropdown-item-title">
+                               John Pierce
+                               <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                           </h3>
+                           <p class="text-sm">I got your message bro</p>
+                           <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                       </div>
+                   </div>
+                   <!-- Message End -->
+               </a>
+               <div class="dropdown-divider"></div>
+               <a href="#" class="dropdown-item">
+                   <!-- Message Start -->
+                   <div class="media">
+                       <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                       <div class="media-body">
+                           <h3 class="dropdown-item-title">
+                               Nora Silvester
+                               <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                           </h3>
+                           <p class="text-sm">The subject goes here</p>
+                           <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                       </div>
+                   </div>
+                   <!-- Message End -->
+               </a>
+               <div class="dropdown-divider"></div>
+               <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+           </div>
+       </li>--}}
+    <!-- Notifications Dropdown Menu -->
         <li class="nav-item dropdown">
-            <a class="nav-link"  href="{{route('article.index',['status' => "ASSIGNED","database" => "maria"])}}">
+            <a class="nav-link" href="{{route('article.index',['status' => "ASSIGNED","database" => "maria"])}}">
                 <i class="far fa-bell" style="width: 20px;"></i>
                 @php
                     $assignedCount = \Modules\Admin\Entities\Article::where('editor_id', \Illuminate\Support\Facades\Auth::user()->id)->where('status', \App\Parafesor\Constants\ArticleStatus::ASSIGNED)->count()
                 @endphp
-                <span class="badge  navbar-badge {{$assignedCount > 0 ? 'badge-danger' : 'badge-success'}} text-sm ">{{$assignedCount}}</span>
+                <span
+                    class="badge  navbar-badge {{$assignedCount > 0 ? 'badge-danger' : 'badge-success'}} text-sm ">{{$assignedCount}}</span>
             </a>
         </li>
         <li class="nav-item">
@@ -120,11 +121,11 @@
                 <i class="fas fa-expand-arrows-alt"></i>
             </a>
         </li>
-     {{--   <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                <i class="fas fa-th-large"></i>
-            </a>
-        </li>--}}
+        {{--   <li class="nav-item">
+               <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+                   <i class="fas fa-th-large"></i>
+               </a>
+           </li>--}}
     </ul>
 </nav>
 <!-- /.navbar -->
@@ -265,7 +266,8 @@
                                     Genel Rapor
                                 </p>
                             </a>
-                            <a href="{{route('bot.report', [\Carbon\Carbon::now()->format('Y-m-d'),\Carbon\Carbon::yesterday()->format('Y-m-d')])}}" class="nav-link active">
+                            <a href="{{route('bot.report', [\Carbon\Carbon::now()->format('Y-m-d'),\Carbon\Carbon::yesterday()->format('Y-m-d')])}}"
+                               class="nav-link active">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Bot Performans</p>
                             </a>
@@ -276,63 +278,65 @@
                 </li>
 
 
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Editor Raporları
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{route('editor.report')}}" class="nav-link">
-                                <i class="nav-icon fas fa-tree"></i>
-                                <p>
-                                    Genel Rapor
-                                </p>
-                            </a>
-                            <a href="{{route('editor.log')}}" class="nav-link active">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Editor Log</p>
-                            </a>
-                        </li>
+                @can('assign articles')
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>
+                                Editor Raporları
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{route('editor.report')}}" class="nav-link">
+                                    <i class="nav-icon fas fa-tree"></i>
+                                    <p>
+                                        Genel Rapor
+                                    </p>
+                                </a>
+                                <a href="{{route('editor.log')}}" class="nav-link active">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Editor Log</p>
+                                </a>
+                            </li>
 
-                    </ul>
-                </li>
-
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Sistem
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{route('articleType.index')}}" class="nav-link">
-                                <i class="nav-icon fas fa-tree"></i>
-                                <p>
-                                    Kategoriler
-                                </p>
-                            </a>
-                            <a href="{{route('system_user.index')}}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Kullanıcılar</p>
-                            </a>
-                            <a href="{{route('system.company.index')}}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Şirketler</p>
-                            </a>
-                            <a href="{{route('system.menu.index')}}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Menu</p>
-                            </a>
-                        </li>
-
-                    </ul>
-                </li>
+                        </ul>
+                    </li>
+                @endcan
+                @can('assign articles')
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>
+                                Sistem
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{route('articleType.index')}}" class="nav-link">
+                                    <i class="nav-icon fas fa-tree"></i>
+                                    <p>
+                                        Kategoriler
+                                    </p>
+                                </a>
+                                <a href="{{route('system_user.index')}}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Kullanıcılar</p>
+                                </a>
+                                <a href="{{route('system.company.index')}}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Şirketler</p>
+                                </a>
+                                <a href="{{route('system.menu.index')}}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Menu</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                @endcan
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
