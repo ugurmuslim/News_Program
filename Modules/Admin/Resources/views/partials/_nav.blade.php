21b1bdeaa25118fdff15401d977fa065e3515abf
Ugur Muslim <ugur.muslim@gmail.com>
2021-12-22 16:41:58 +0300

    continue

--- Modules/Admin/Resources/views/partials/_nav.blade.php
+++ Modules/Admin/Resources/views/partials/_nav.blade.php
@@ -145,7 +145,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <i class="fas fa-male mt-2" style="color: white"></i>
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{Auth::user()->name}}</a>
