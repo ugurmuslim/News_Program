cf2e9b73cb29be2fc86cd346d234133782ebe9e3
Ugur Muslim <ugur.muslim@gmail.com>
2021-12-26 21:21:00 +0300

    continue

---
+++ app/Console/Commands/ShowArticles.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Parafesor\Constants\CacheConst;
+use App\Parafesor\Constants\CategorySectionTypes;
+use App\Parafesor\Youtube\YoutubeCrawler;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Ramsey\Collection\CollectionInterface;
+
+class ShowArticles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'show:articles';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return CollectionInterface
+     */
+    public function handle()
+    {
+        dd(Cache::get(CacheConst::ARTICLE . 'Kripto' . ":" . CategorySectionTypes::NORMAL));
+    }
+}
