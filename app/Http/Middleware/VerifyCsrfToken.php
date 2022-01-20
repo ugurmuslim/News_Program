9fc5b67b57e312818890532e30fe009391b14574
Ugur Muslim <ugur.muslim@gmail.com>
2022-01-20 23:20:50 +0300

    continue

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         '/admin/article/editor/image',
         '/admin/article/preview',
+        '/admin/article/postUpdate',
         '/home/sliders'
     ];
 }
