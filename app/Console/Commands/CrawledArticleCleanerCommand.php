12be9e13c5629b756af0abb1778fcbcfc1aa2df2
Ugur Muslim <ugur.muslim@gmail.com>
2022-02-06 19:40:15 +0300

    continue

---
+++ app/Console/Commands/CrawledArticleCleanerCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Modules\Admin\Entities\CrawledArticle;
+
+class CrawledArticleCleanerCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crawled:cleaner';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        CrawledArticle::where('created_at','<', Carbon::now()->subDays(7))->delete();
+    }
+}
