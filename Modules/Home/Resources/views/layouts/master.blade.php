0325ae758f0b2bb03f98c74b64a5cb693fce4276
Ugur Muslim <ugur.muslim@gmail.com>
2022-02-08 19:36:10 +0300

    continu

--- Modules/Home/Resources/views/layouts/master.blade.php
+++ Modules/Home/Resources/views/layouts/master.blade.php
@@ -75,7 +75,7 @@
 
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.css" />
-    <script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.js" async></script>
+    <script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
         integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async>
     </script>
