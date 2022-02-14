7bb0a5ca643a23d665a15c9f4e2c07a87b45cb3a
Ugur Muslim <ugur.muslim@gmail.com>
2022-02-14 23:47:19 +0300

    continue

--- Modules/Home/Resources/views/Article/show.blade.php
+++ Modules/Home/Resources/views/Article/show.blade.php
@@ -1,6 +1,7 @@
 @extends('home::layouts.master')
 @section('extra_css')
-    <link href="{{ asset('modules/home/sample/css/detail.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('modules/home/sample/css/detail.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('modules/home/sample/css/footer.css') }}" rel="stylesheet" type="text/css"/>
 @endsection
 @section('title', $article->title . " | Parafesör | Paranıza Akıl Verir")
 @section('seo_description', $article->seo_description)
@@ -186,44 +187,120 @@
             </div>
         </div>
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-  {{--  <div class="container">
-        <div class="col-md-16 offset-md-4">
-            <h1 style="display: block; text-align: center">{{$article->title}}</h1>
-        </div>
-        <div class="image mt-5 mx-auto d-block" style="max-width: 1000px;">
-            <img
-                src="{{asset($article->image_path)}}"
-                class="rounded mx-auto d-block img-fluid" alt="...">
-        </div>
-
-        <div class="container">
-            <div class="col-md-16 offset-md-4 mt-5">
-                <div class="row summary ">
-            <span class="font-italic">
-                {!! $article->summary !!}
-            </span>
+    <section id="section-footer">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5 col-sm-12 col-24">
+                        <a href="#"><img src="sample/img/parafesor-logo-light.svg" alt="Parafesör Light Logo"></a>
+                        <p>Paranıza Akıl Verir<br>Gündemin en önemli haberleri Parafesör’de! Borsa, hisse, şirket,
+                            kripto, teknoloji ve yaşam haberleri.</p>
+                        <p>İletişim: <a href="mailto:info@parafesor.net">info@parafesor.net</a></p>
+                        <div class="social-icons">
+                            <a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fab fa-youtube" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-sm-12 col-24">
+                        <h4>PARAFESÖR</h4>
+                        <ul>
+                            <li><a href="#">Gündem Haberleri</a></li>
+                            <li><a href="#">Borsa Tube</a></li>
+                            <li><a href="#">Twitter Yazıları</a></li>
+                            <li><a href="#">Şirket Haberleri</a></li>
+                            <li><a href="#">Borsa Haberleri</a></li>
+                            <li><a href="#">Kripto Para Haberleri</a></li>
+                            <li><a href="#">Hisse Önerileri</a></li>
+                            <li><a href="#">Köşe Yazarları</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-5 col-sm-12 col-24">
+                        <h4>PARAFESÖR STİL</h4>
+                        <ul>
+                            <li><a href="#">Spor</a></li>
+                            <li><a href="#">Teknoloji</a></li>
+                            <li><a href="#">Yaşam</a></li>
+                            <li><a href="#">Netkolik</a></li>
+                            <li><a href="#">Otomobil</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-5 col-sm-12 col-24">
+                        <h4>PİYASALAR</h4>
+                        <ul>
+                            <li><a href="#">Döviz Piyasaları</a></li>
+                            <li><a href="#">Dolar Haberleri</a></li>
+                            <li><a href="#">Euro Haberleri</a></li>
+                            <li><a href="#">Emtia Haberleri</a></li>
+                            <li><a href="#">Bist100 Haberleri</a></li>
+                            <li><a href="#">Bist30 Haberleri</a></li>
+                            <li><a href="#">Viop Haberleri</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-5 col-sm-12 col-24">
+                        <h4>HİSSE RAPORLARI</h4>
+                        <ul>
+                            <li><a href="#">Zirvedeki Hisseler</a></li>
+                            <li><a href="#">Hisse Raporları</a></li>
+                            <li><a href="#">Aracı Kurum Tavsiyeleri</a></li>
+                            <li><a href="#">Net Karı En Fazla Artan Şirketler</a></li>
+                            <li><a href="#">Karı Sermayelerinden Yüksek Şirketler</a></li>
+                            <li><a href="#">Son 5 Yılda Kaybettirmeyen Hisseler</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="news-card-bottom mt-5">
-            <span
-                class="text-danger">{{ Date::parse($article->article_date)->format('j F')}}</span><span> • {{ Carbon\Carbon::parse($article->article_date)->format('H:i')}} • parafesor</span>
-                </div>
-                <div class="row mt-5">
-                    {!! $article->body !!}
-
+                <div class="row copyright">
+                    <div class="col-left col-lg-12 col-sm-24">
+                        <span>Parafesör 2022 © Tüm Hakları Saklıdır.</span>
+                        <span>Tasarım: <a href="#" target="_blank">emrealkac.com</a></span>
+                    </div>
+                    <div class="col-right col-lg-12 col-sm-24">
+                        <a href="#"><img src="{{asset('modules/home/sample/img/anadolu-ajansi-logo.svg')}}" target="_blank"
+                                         alt="Anadolu Ajansı Logo"></a>
+                        <a href="#"><img src="{{asset('modules/home/sample/img/foreks-logo.svg')}}" target="_blank" alt="Foreks Logo"></a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>--}}
+        </footer>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+    {{--  <div class="container">
+          <div class="col-md-16 offset-md-4">
+              <h1 style="display: block; text-align: center">{{$article->title}}</h1>
+          </div>
+          <div class="image mt-5 mx-auto d-block" style="max-width: 1000px;">
+              <img
+                  src="{{asset($article->image_path)}}"
+                  class="rounded mx-auto d-block img-fluid" alt="...">
+          </div>
+
+          <div class="container">
+              <div class="col-md-16 offset-md-4 mt-5">
+                  <div class="row summary ">
+              <span class="font-italic">
+                  {!! $article->summary !!}
+              </span>
+                  </div>
+                  <div class="news-card-bottom mt-5">
+              <span
+                  class="text-danger">{{ Date::parse($article->article_date)->format('j F')}}</span><span> • {{ Carbon\Carbon::parse($article->article_date)->format('H:i')}} • parafesor</span>
+                  </div>
+                  <div class="row mt-5">
+                      {!! $article->body !!}
+
+                  </div>
+              </div>
+          </div>
+      </div>--}}
 @endsection
