4c21c6d7fbc514ff5fe074ae57d6cc1ffac857cb
ugur muslim <ugur.muslim@gmail.com>
2022-03-13 10:18:04 +0300

    continue

--- Modules/Home/Resources/views/partials/_javascript.blade.php
+++ Modules/Home/Resources/views/partials/_javascript.blade.php
@@ -3,5 +3,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
 integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
 crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
 
