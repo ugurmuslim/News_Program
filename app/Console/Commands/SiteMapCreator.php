4ed8a40ec459684539bc7ef479260dfcddec410c
Eren İlhan <erenilhan1@gmail.com>
2022-03-16 00:06:24 +0300

    sitemap devam

--- app/Console/Commands/SiteMapCreator.php
+++ app/Console/Commands/SiteMapCreator.php
@@ -2,10 +2,12 @@
 
 namespace App\Console\Commands;
 
-use App\Parafesor\FeedParser\FeedParserHelper;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
-use Spatie\Sitemap\SitemapGenerator;
-
+use Modules\Admin\Entities\Article;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\SitemapIndex;
+use Spatie\Sitemap\Tags\Url;
 class SiteMapCreator extends Command
 {
 		/**
@@ -39,6 +41,58 @@ class SiteMapCreator extends Command
 		 */
 		public function handle()
 		{
-				SitemapGenerator::create('https://parafesor.net')->writeToFile('public/sitemap.xml');
+				$now = Carbon::now();
+				$sitemap = Sitemap::create();
+
+				$articles = Article::whereMonth('created_at', $now->month)
+					->whereNotNull('slug')
+					->get();
+
+				$path=public_path('/sitemaps/' . $now->year . '-' . $now->month . '.xml');
+				foreach ($articles as $article) {
+						$sitemap->add(Url::create(url($article->slug))
+							->setChangeFrequency(Url::CHANGE_FREQUENCY_HOURLY));
+				}
+
+				$sitemap->writeToFile($path);
+
+				$sitemapIndex = SitemapIndex::create();
+
+				$articlesChunk = Article::select(['slug', 'updated_at'])
+					->chunk(50000, function ($articles, $chunk) use ($sitemapIndex) {
+							$sitemapName = 'articles_sitemap_'.$chunk.'.xml';
+							$sitemap = Sitemap::create();
+
+							foreach ($articles as $article) {
+									$sitemap->add(Url::create($article->slug)
+										->setLastModificationDate($article->updated_at));
+							}
+
+							$sitemap->writeToFile(public_path($sitemapName));
+							$sitemapIndex->add($sitemapName);
+					});
+
+				$sitemapIndex->writeToFile(public_path('sitemap.xml'));
+
+//				Sitemap::create()->add(Url::create('/sitemaps/' . $now->year . '-' . $now->month . '.xml'))
+//					->writeToFile(public_path('sitemap.xml'));
+
+//				SitemapGenerator::create(config('app.url'))
+//					->maxTagsPerSitemap(10)
+//					->writeToFile(public_path('sitemap.xml'));
+//				$sitemap = Sitemap::create()->maxTagsPerSitemap(2);
+//				$articles = Article::all();
+//
+//				foreach ($articles as $article) {
+//						$sitemap->add(Url::create(url($article->slug))
+//							->setChangeFrequency(Url::CHANGE_FREQUENCY_HOURLY));
+//				}
+//
+//				$sitemap->writeToFile(public_path('/sitemaps/' . $now->year . '-' . $now->month . '.xml'));
+//
+//
+//				Sitemap::create()->add(Url::create('/sitemaps/' . $now->year . '-' . $now->month . '.xml'))
+//					->writeToFile(public_path('sitemap.xml'));
 		}
 }
+
