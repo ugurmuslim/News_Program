8d9e1ef9704cc36a3db54ae210f297068031d1d3
ugur muslim <ugur.muslim@gmail.com>
2022-03-18 23:48:30 +0300

    commenting index migraiont

--- database/migrations/2022_03_15_223931_add_index_to_articles_table.php
+++ database/migrations/2022_03_15_223931_add_index_to_articles_table.php
@@ -8,11 +8,11 @@ class AddIndexToArticlesTable extends Migration
 {
 		public function up()
 		{
-				Schema::table('articles', function (Blueprint $table) {
+			/*	Schema::table('articles', function (Blueprint $table) {
 						$table->index(['article_type_id', 'status', 'persistent', 'start_date', 'show_case', 'article_date', 'header_slider'], 'idxArticlesByType');
 						$table->index('read');
 						$table->index('slug');
-				});
+				});*/
 		}
 
 		public function down()
--- database/migrations/2022_03_15_225342_add_index_to_stock_tubes_table.php
+++ database/migrations/2022_03_15_225342_add_index_to_stock_tubes_table.php
@@ -8,15 +8,16 @@ class AddIndexToStockTubesTable extends Migration
 {
 		public function up()
 		{
-				Schema::table('stock_tube', function (Blueprint $table) {
+			/*	Schema::table('stock_tube', function (Blueprint $table) {
 						$table->index(['created_at']);
 						$table->index(['show_case']);
 						$table->index('status');
-				});
+				});*/
 		}
 
 		public function down()
 		{
+            dd(1);
 				Schema::table('stock_tube', function (Blueprint $table) {
 						$table->dropIndex(['created_at']);
 						$table->dropIndex(['show_case']);
